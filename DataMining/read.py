323f32b04b580a8c6262fa7e851ac04dde761f34
Péricles Borges <92488227+Jerry-523@users.noreply.github.com>
2023-12-19 14:02:31 +0000

    Update read.py

--- DataMining/read.py
+++ DataMining/read.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-# Carregar os dados em DataFrames do Pandas
 account_data = pd.read_csv('dataset/account.asc')  
 client_data = pd.read_csv('dataset/client.asc')    
 card_data = pd.read_csv('dataset/card.asc')
@@ -9,7 +8,6 @@ order_data = pd.read_csv('dataset/order.asc')
 district_data = pd.read_csv('dataset/district.asc')
 disp_data = pd.read_csv('dataset/disp.asc')
 
-# Exibindo as primeiras linhas de cada DataFrame para verificar se os dados foram carregados corretamente
 print("Dados da Conta:")
 print(account_data.head())
 
